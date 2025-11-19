24c18d1076ebc91f114e0968a8bcd586595fa767
Foysal-Munsy <bdfoysal555@gmail.com>
2025-11-19 21:52:12 +0600

    feat(auth) added login process by comparing hashed password and credentials.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -13,5 +14,10 @@ export class AuthController {
     createUserDto.password = hashedPassword;
     createUserDto['role'] = 'user'; // Assign default role
     return this.authService.register(createUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from 'typeorm';
+import { LoginUserDto } from './dto/login-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,14 +16,25 @@ export class AuthService {
     const existingUser = await this.userRepository.findOne({
       where: { email: createUserDto.email },
     });
-
     if (existingUser) {
       throw new ConflictException(
         'User already exists, please log in or use new credentials.',
       );
     }
-
     const newUser = this.userRepository.create(createUserDto);
     return this.userRepository.save(newUser);
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<User | null> {
+    const user = await this.userRepository.findOne({
+      where: { email: loginUserDto.email },
+    });
+    if (!user) {
+      throw new ConflictException('Please log in or use new credentials.');
+    }
+    if (!(await bcrypt.compare(loginUserDto.password, user.password))) {
+      throw new ConflictException('Invalid credentials provided.');
+    }
+    return user;
   }
 }
