ec7714d11bafc00daee925f462490f33d42c28b1
Foysal-Munsy <bdfoysal555@gmail.com>
2025-11-19 20:58:04 +0600

    [modified] register for handling multiple user.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -11,6 +11,16 @@ export class AuthService {
   ) {}
 
   async register(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException(
+        'User already exists, please log in or use new credentials.',
+      );
+    }
+
     const newUser = this.userRepository.create(createUserDto);
     return this.userRepository.save(newUser);
   }
